7210dcbc90a5ff6755131798ac75186e457f4078
Ubuntu <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2014-03-22 12:18:33 +0000

    trying again

--- web.js
+++ web.js
@@ -3,14 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  text_buffer = fs.readFileSync('index.html', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-  });
-  
+  text_buffer = fs.readFile('index.html');
   text_string = text_buffer.toString();
   response.send(text_string);
-}
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
