ae9776c6dcbe9ebfb74eae48afe13cfca0561f5c
Ubuntu <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2014-03-22 18:04:09 +0000

    more

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
-text_buffer = fs.readFile('index.html');
-text_string = text_buffer.toString();
+//text_buffer = fs.readFile('index.html');
+//text_string = text_buffer.toString();
 
 
 app.get('/', function(request, response) {
-  response.send(text_string);
+  //response.send(text_string);
+  response.send("test")
 });
 
 var port = process.env.PORT || 5000;
