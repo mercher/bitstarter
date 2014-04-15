8a70e7d56e3c08739905721c00f844bde7961972
mercher <jjsm76@gmail.com>
2014-04-15 17:51:34 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
   response.send(text_string);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
